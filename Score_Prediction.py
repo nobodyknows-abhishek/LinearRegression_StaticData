9f43332ebdbb004dbc33e5e2b1748da3e4a0716c
Abhishek Verma <130924459+nobodyknows-abhishek@users.noreply.github.com>
2024-06-13 22:41:14 +0530

    Score_Prediction.py

--- Score_Prediction.py
+++ Score_Prediction.py
@@ -18,8 +18,6 @@ y_pred=model.predict(X_test)
 mae=mean_absolute_error(y_test,y_pred)
 mse=mean_squared_error(y_test,y_pred)
 r2=r2_score(y_test,y_pred)
-# marks_scored = np.array([7]).reshape(-1,1)
-# predicted_score = model.predict(marks_scored)
 
 print(f' MAE:{mae}, MSE:{mse}, R-squared:{r2}')
 new_data=[[7]]
